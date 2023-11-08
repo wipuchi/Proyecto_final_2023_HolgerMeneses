2d2cb7cc4c11a8bed64d1230202593b4847087be
Holger Andres Meneses Jaramillo <holgerandresmj@hotmail.com>
2023-11-08 09:03:49 -0500

    commit 1
    Creation of class with the arrays, and jframe form.

---
+++ Proyecto_final_2023_HolgerMeneses/src/app_state.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class app_state {
+    static cls_state_register [] state = new cls_state_register[100];
+    static cls_allotment [] allotment = new cls_allotment[100];
+
+    static int int_position = 0;
+}
